e81be3199b93028c36b7222c005b0c438eb6943f
Nikhil Kanse <31060277+nikhilkanse@users.noreply.github.com>
2017-11-21 17:34:24 +0530

    Change folder structure.

--- app/src/main/java/com/in/nyk/remindme/activity/MainActivity.java
+++ app/src/main/java/com/in/nyk/remindme/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.in.nyk.remindme;
+package com.in.nyk.remindme.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.in.nyk.remindme.R;
 
 public class MainActivity extends AppCompatActivity {
 
