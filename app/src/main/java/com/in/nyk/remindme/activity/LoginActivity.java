4c087092de474ba615d7c06dceeb4df9378dc636
Nikhil Kanse <31060277+nikhilkanse@users.noreply.github.com>
2017-11-22 12:23:36 +0530

    Change in gradle files.

---
+++ app/src/main/java/com/in/nyk/remindme/activity/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.in.nyk.remindme.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.in.nyk.remindme.R;
+
+/**
+ * Created by nikhilkanse on 21/11/17.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
